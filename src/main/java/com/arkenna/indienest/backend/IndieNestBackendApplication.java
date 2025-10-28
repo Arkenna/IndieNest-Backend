0134f26969bef5366886bd41d45ca787a564e9fc
Daniel Aquino <u202217678@upc.edu.pe>
2025-10-28 15:59:40 -0500

    chore: initial commit.

---
+++ src/main/java/com/arkenna/indienest/backend/IndieNestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arkenna.indienest.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndieNestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndieNestBackendApplication.class, args);
+    }
+
+}
