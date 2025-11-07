68e8ac2795c256711a1dc3b093cd745220eecb05
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-07 17:59:44 -0500

    feat(project): add art category value object

---
+++ src/main/java/com/arkenna/indienest/backend/project/domain/model/valueobjects/ArtCategory.java
@@ -0,0 +1,13 @@
+package com.arkenna.indienest.backend.project.domain.model.valueobjects;
+
+/**
+ * ArtCategory Value Object
+ */
+public enum ArtCategory {
+    PIXEL_ART,
+    POSTER,
+    FAN_ART,
+    CHARACTER_DESIGN,
+    CONCEPT_ART,
+    BACKGROUND_DESIGN
+}
