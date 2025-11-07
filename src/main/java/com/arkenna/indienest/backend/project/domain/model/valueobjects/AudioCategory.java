b8284ce63d4d51042856b82491fab4bd837dc2ae
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-07 17:58:08 -0500

    feat(project): add audio category value object

---
+++ src/main/java/com/arkenna/indienest/backend/project/domain/model/valueobjects/AudioCategory.java
@@ -0,0 +1,12 @@
+package com.arkenna.indienest.backend.project.domain.model.valueobjects;
+
+/**
+ * AudioCategory Value Object
+ */
+public enum AudioCategory {
+    SOUNDTRACK,
+    INSTRUMENTAL,
+    VOICE_ACTING,
+    SOUND_EFFECT,
+    OTHER
+}
