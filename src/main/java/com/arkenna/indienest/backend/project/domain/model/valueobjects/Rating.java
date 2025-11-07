37759ad8cb37ade859582c6e80f85d3b1a35af78
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-07 17:37:24 -0500

    feat(project): add rating value object

---
+++ src/main/java/com/arkenna/indienest/backend/project/domain/model/valueobjects/Rating.java
@@ -0,0 +1,16 @@
+package com.arkenna.indienest.backend.project.domain.model.valueobjects;
+
+/**
+ * Rating Value Object
+ */
+public record Rating(Integer rating) {
+
+    /**
+     * Constructor with validation
+     * @param rating Rating
+     */
+    public Rating {
+        if(rating < 0)
+            throw new IllegalArgumentException("Invalid rating");
+    }
+}
