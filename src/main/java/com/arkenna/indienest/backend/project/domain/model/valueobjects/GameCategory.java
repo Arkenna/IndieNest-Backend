149a30d47062d41cb5cf8a55861ba05ca354a278
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-07 18:01:12 -0500

    feat(project): add game category value object

---
+++ src/main/java/com/arkenna/indienest/backend/project/domain/model/valueobjects/GameCategory.java
@@ -0,0 +1,15 @@
+package com.arkenna.indienest.backend.project.domain.model.valueobjects;
+
+/**
+ * GameCategory Value Object
+ */
+public enum GameCategory {
+    HORROR,
+    SURVIVAL,
+    FPS,
+    METROIDVANIA,
+    SANDBOX,
+    PUZZLE,
+    PLATFORMER,
+    RPG
+}
