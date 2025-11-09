2ebf09f0aa13b953eba09cd0324c576b6f8871a8
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-09 13:51:08 -0500

    feat(payment): add acl and service implementation

--- src/main/java/com/arkenna/indienest/backend/payment/domain/model/commands/AddGameToShoppingCartCommand.java
+++ src/main/java/com/arkenna/indienest/backend/payment/domain/model/commands/AddGameToShoppingCartCommand.java
@@ -1,8 +1,10 @@
 package com.arkenna.indienest.backend.payment.domain.model.commands;
 
 
+import java.math.BigDecimal;
+
 /**
  * Add Game To Shopping Cart Command
  */
-public record AddGameToShoppingCartCommand(Integer shoppingCartId, Integer gameId) {
+public record AddGameToShoppingCartCommand(Integer shoppingCartId, Integer gameId, BigDecimal price) {
 }
--- src/main/java/com/arkenna/indienest/backend/payment/domain/model/commands/DeleteGameFromShoppingCartCommand.java
+++ src/main/java/com/arkenna/indienest/backend/payment/domain/model/commands/DeleteGameFromShoppingCartCommand.java
@@ -1,7 +1,9 @@
 package com.arkenna.indienest.backend.payment.domain.model.commands;
 
+import java.math.BigDecimal;
+
 /**
  * Delete Game From ShoppingCart Command
  */
-public record DeleteGameFromShoppingCartCommand(Integer shoppingCartId, Integer gameId) {
+public record DeleteGameFromShoppingCartCommand(Integer shoppingCartId, Integer gameId, BigDecimal price) {
 }
